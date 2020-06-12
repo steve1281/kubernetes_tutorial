0295be3b08391980545a8be1cdba137e9347c7f2
steve1281 <steve1281@hotmail.com>
2020-06-12 09:13:01 -0400

    modify server to use the singleton Logger

--- flask-logger/server.py
+++ flask-logger/server.py
@@ -2,15 +2,17 @@ from flask import Flask
 from logger import Logger
 
 app = Flask(__name__)
-logger = Logger(app, "log.log")
 
 @app.route('/')
 def foo():
+    logger = Logger()
+    print(logger)
     logger.warning('A warning occurred (%d apples)' % 42)
     logger.error('An error occurred')
     logger.info('Info')
     return "foo"
 
 if __name__ == '__main__':
-    logger.info("starting flask...")
+    logger = Logger(app, "log.log")
+    logger.warning("starting flask...")
     app.run()
